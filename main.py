5df69f5756efe056158d1a3b37289b5dbc884ef2
mangostin2010 <133020818+mangostin2010@users.noreply.github.com>
2024-04-23 08:27:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ girls = get_student('girl')
 students = boys | girls
 if sort_type == 'Average':
     #students = dict(sorted(students.items()))
-    sy.write(sorted(students.items()))
+    st.write(sorted(students.items()))
 # Listing Students ------------------------------------------------------------------------
 
 # Chunking students into groups of 3
